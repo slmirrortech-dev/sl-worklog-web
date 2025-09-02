7cde1b6999864f8623288d6d7d395a8754f85e53
kkomyoung <kya754@gmail.com>
2025-09-02 16:52:28 +0900

    chore: 스웨거 셋팅 수정

--- lib/swagger.ts
+++ lib/swagger.ts
@@ -10,8 +10,8 @@ const options = {
     },
     servers: [
       {
-        url: 'http://localhost:3000',
-        description: 'Development server',
+        url: process.env.NEXTAUTH_URL || 'http://localhost:3000',
+        description: process.env.NODE_ENV === 'production' ? 'Production server' : 'Development server',
       },
     ],
     components: {
