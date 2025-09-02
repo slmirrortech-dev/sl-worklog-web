fdbdcd7a02ae1e44e3f94a2e25a4db5cb4e9768e
kkomyoung <kya754@gmail.com>
2025-09-02 14:16:36 +0900

    fix(prettier): format prisma client

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,12 +1,17 @@
-// src/lib/prisma.ts
 import { PrismaClient } from '@prisma/client'
 
-const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient }
+declare global {
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined
+}
 
-export const prisma =
-  globalForPrisma.prisma ??
+const prismaClientSingleton = () =>
   new PrismaClient({
     log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
   })
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+export const prisma = globalThis.prisma ?? prismaClientSingleton()
+
+if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma
+
+export default prisma
